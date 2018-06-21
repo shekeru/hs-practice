a7470e0b608608e73f04d72ca25b7a22c86e64d6
Sheky Sheks <sheks@desu.systems>
2018-06-21 00:39:52 -0400

    correct sign

--- Problem 4.hs
+++ Problem 4.hs
@@ -9,7 +9,7 @@ palindrome n = reverse (show n) == show n && validFactors n
 validFactors :: Integer -> Bool
 validFactors n = let r = (floor.sqrt.fromIntegral) n in
   isTriple $ head [[x, n `div` x] | x <- [r,r-1..2], n `mod` x == 0]
-  where isTriple = all (\y -> y > i `div` 10 && y < i + 1)
+  where isTriple = all (\y -> y > i `div` 10 && y <= i + 1)
 
 i :: Integer
 i = 999
