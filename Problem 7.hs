468f33762c72f0309878ffb6fe614925555e7ca3
Sheky Sheks <sheks@desu.systems>
2018-06-22 15:02:33 -0400

    sieve example

--- Problem 7.hs
+++ Problem 7.hs
@@ -13,5 +13,6 @@ primesTo 2 = [2]
 primesTo n = [x | x<-[2..n], all ((> 0).rem x) $ prev x]
   where prev = primesTo.truncate.sqrt.fromIntegral
 
-primes = do
-  
+import Data.List.Ordered (minus, union, unionAll)
+
+primes = 2 : 3 : minus [5,7..] (unionAll [[p*p, p*p+2*p..] | p <- tail primes])  
