428fa170ecd0c01b88958f5e280111759d91b32c
Sheky Sheks <sheks@desu.systems>
2018-11-28 18:18:02 -0500

    lol

--- Problem 13.hs
+++ Problem 13.hs
@@ -2,3 +2,15 @@ module Main where
 
 import Control.Monad
 import Data.List
+import Data.Char
+
+main = do
+  nxs <- numbers; let
+    (l,s) = foldr leftward (0, "") nxs
+  putStrLn.take 10 $ show l ++ s
+
+numbers = map(sum.map digitToInt).transpose.lines <$> readFile "Problem 13.txt"
+
+leftward :: Int -> (Int, String) -> (Int, String)
+leftward n (r,s) = let i = n+r in (div i 10,
+  (intToDigit.rem i) 10 : s)
